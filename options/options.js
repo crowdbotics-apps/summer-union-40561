7f26ada52523ddca68322dfc3b4c80732eaee328
Crowdbotics <team@crowdbotics.com>
2023-05-08 23:38:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "summer-union-40561",
+  url: "https://summer-union-40561.botics.co",
+  api: "https://summer-union-40561.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
